710b545a2327334a9ac98c5d6af84dc21922cc2a
Yves Senn <yves.senn@gmail.com>
2012-01-08 16:05:39 +0100

    macros to eliminate duplication in integration specs

--- spec/integration/update_records_spec.rb
+++ spec/integration/update_records_spec.rb
@@ -2,56 +2,47 @@ require 'data-import'
 
 describe "update existing records" do
 
-  let(:plan) do
-    DataImport::Dsl.define do
-      source :sequel, 'sqlite:/'
-      target :sequel, 'sqlite:/'
+  in_memory_mapping do
+    import 'Article Authors' do
+      from 'tblArticleAbout', :primary_key => 'sID'
+      to 'articles', :mode => :update
 
-      source_database.db.create_table :tblArticleAbout do
-        primary_key :sID
-        Integer :lArticleId
-        String :strWho
-      end
-
-      target_database.db.create_table :articles do
-        primary_key :id
-        String :title
-        String :author
-      end
-
-      import 'Article Authors' do
-        from 'tblArticleAbout', :primary_key => 'sID'
-        to 'articles', :mode => :update
-
-        mapping 'lArticleId' => 'id'
-        mapping 'strWho' => 'author'
-      end
+      mapping 'lArticleId' => 'id'
+      mapping 'strWho' => 'author'
     end
   end
 
-  let(:source) { plan.definitions.first.source_database.db[:tblArticleAbout] }
-  let(:target) { plan.definitions.first.target_database.db[:articles] }
+  database_setup do
+    source.create_table :tblArticleAbout do
+      primary_key :sID
+      Integer :lArticleId
+      String :strWho
+    end
 
-  before do
-    source.insert(:sID => 1,
-                  :lArticleId => 12,
-                  :strWho => 'Adam K.')
-    source.insert(:sID => 2,
-                  :lArticleId => 145,
-                  :strWho => 'James G.')
+    target.create_table :articles do
+      primary_key :id
+      String :title
+      String :author
+    end
 
-    target.insert(:id => 12,
-                  :title => 'The Book!')
-    target.insert(:id => 145,
-                  :title => 'The other Book.')
+    source[:tblArticleAbout].insert(:sID => 1,
+                                    :lArticleId => 12,
+                                    :strWho => 'Adam K.')
+    source[:tblArticleAbout].insert(:sID => 2,
+                                    :lArticleId => 145,
+                                    :strWho => 'James G.')
+
+    target[:articles].insert(:id => 12,
+                             :title => 'The Book!')
+    target[:articles].insert(:id => 145,
+                             :title => 'The other Book.')
 
   end
 
-
   it 'mapps columns to the new schema' do
     DataImport.run_plan!(plan)
-    target.to_a.should == [{:id => 12, :title => "The Book!", :author => 'Adam K.'},
-                           {:id => 145, :title => "The other Book.", :author => 'James G.'}]
+    target_database[:articles].to_a.should == [{:id => 12, :title => "The Book!", :author => 'Adam K.'},
+                                               {:id => 145, :title => "The other Book.", :author => 'James G.'}]
   end
 
 end
