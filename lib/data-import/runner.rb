990be1b071a620a90b90f73f20042ef4d9f64dc5
Yves Senn <yves.senn@gmail.com>
2012-01-13 22:27:05 +0100

    pass the execution plan as context in place of the runner

--- lib/data-import/runner.rb
+++ lib/data-import/runner.rb
@@ -11,18 +11,9 @@ module DataImport
       resolved_plan = dependency_resolver.resolve(:run_only => options[:only])
       resolved_plan.definitions.each do |definition|
         bar = @progress_reporter.new(definition.name, definition.total_steps_required)
-        definition.run(self, bar)
+        definition.run(@plan, bar)
         bar.finish
       end
     end
-
-    def definition(name)
-      @plan.definition(name)
-    end
-
-    def before_filter
-      @plan.before_filter
-    end
-
   end
 end
