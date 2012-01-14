429d4a27ccf1c157760d0955a7b6fd5f89c9334b
Yves Senn <yves.senn@gmail.com>
2012-01-14 17:37:21 +0100

    remove a monkeypatch which was fixed in activesupport

--- lib/data-import.rb
+++ lib/data-import.rb
@@ -12,17 +12,7 @@ require 'data-import/database'
 require 'data-import/definition'
 require 'data-import/importer'
 
-# Monkeypatch for active support (see https://github.com/rails/rails/pull/2801)
-class Time
-  class << self
-    def ===(other)
-      super || (self == Time && other.is_a?(ActiveSupport::TimeWithZone))
-    end
-  end
-end
-
 module DataImport
-
   def self.run_config!(config_path, options = {})
     plan = DataImport::Dsl.evaluate_import_config(config_path)
     run_plan!(plan, options)
@@ -32,5 +22,4 @@ module DataImport
     runner = Runner.new(plan)
     runner.run(options)
   end
-
 end
