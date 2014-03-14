499e99250ce4d7983b2a683bcd2cf34c4ab9e69e
Robin Wenglewski <robin@wenglewski.de>
2014-03-14 11:30:03 +0100

    disable postgres tests by default

--- spec/integration/spec_helper.rb
+++ spec/integration/spec_helper.rb
@@ -7,4 +7,5 @@ require File.join(File.dirname(__FILE__), '../acceptance/support/macros')
 
 RSpec.configure do |config|
   config.extend TestingMacros
+  config.filter_run_excluding :postgres => true unless ENV.has_key? 'ENABLE_POSTGRES_SPECS'
 end
